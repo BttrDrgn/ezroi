1469c84c2f2c1d84182fe28c26b7e8bfb346eb96
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2021-09-20 01:14:11 -0500

    Invite command

---
+++ commands/invite.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+const { inviteUrl } = require("../config.json");
+
+module.exports =
+{
+	async execute(interaction)
+	{
+		let package = JSON.parse(fs.readFileSync("./package.json"));
+		let github = package.repository.url.slice(4, package.repository.url.length - 4);
+		console.log(github);
+
+		let embed = new MessageEmbed()
+			.setAuthor("EzROI", interaction.client.user.displayAvatarURL())
+			.setDescription(`Invite me to your Discord server!`)
+			.addField("Bot Invite", `[Click me!](${inviteUrl})`, true)
+			.addField("Github", `[Click me!](${github})`, true)
+
+		await interaction.reply({ embeds: [embed], ephemeral: false});
+	},
+
+	data: new SlashCommandBuilder()
+		.setName('invite')
+		.setDescription('Invite me to your server!')
+};
