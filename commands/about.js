0553dbc0bf85a8d56b3ffa0d869ac0dd669b82e8
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2021-09-20 12:13:29 -0500

    Wrong word in about

--- commands/about.js
+++ commands/about.js
@@ -8,7 +8,7 @@ module.exports =
 		let embed = new MessageEmbed()
 			.setAuthor("EzROI", interaction.client.user.displayAvatarURL())
 			.setDescription(`About Me`)
-			.addField("Primary Focus", `EzROI is a helpful math both that attempts to provide accurate data to determine the ROI (Return of Investment) of the GPU provided in terms of mining through Nicehash. By coupling Nicehash data with Coinbase's currency API, the most accurate data at the current moment is provided for every action. While fiat conversions are the main focus, some cryptocurrencies can be provided in the data and may return good results. Due to the extreme amount of cryptocurrencies available, only the most popular cryptos will be accounted for (BTC, ETH, XLM, ADA, ect.).`, false)
+			.addField("Primary Focus", `EzROI is a helpful math both that attempts to provide accurate data to determine the ROI (Return of Investment) of the GPU provided in terms of mining through Nicehash. By coupling Nicehash data with Coinbase's currency API, the most accurate data at the current moment is provided for every action. While fiat currencies are the main focus, some cryptocurrencies can be provided in the data and may return good results. Due to the extreme amount of cryptocurrencies available, only the most popular cryptos will be accounted for (BTC, ETH, XLM, ADA, ect.).`, false)
 			.addField("What It **CAN** Do", 
 			`\u2022 Help you make a decision
 			\u2022 List information about GPUs
