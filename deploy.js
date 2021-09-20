612a44dd5dbd5471da7dc0419215586493cd97d4
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2021-09-20 10:45:56 -0500

    Staging deploy

--- deploy.js
+++ deploy.js
@@ -1,15 +1,33 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.json');
+const { clientId, guildId, token } = require('./config.json');
+
+let staging = process.argv.includes("--staging");
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles)
+if(staging)
 {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+	for (const file of commandFiles)
+	{
+		if(file == "op.json")
+		{
+			continue;
+		}
+		
+		const command = require(`./commands/${file}`);
+		commands.push(command.data.toJSON());
+	}
+}
+else
+{
+	for (const file of commandFiles)
+	{
+		const command = require(`./commands/${file}`);
+		commands.push(command.data.toJSON());
+	}
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
@@ -18,10 +36,20 @@ const rest = new REST({ version: '9' }).setToken(token);
 {
 	try
 	{
-		await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
+		if(staging)
+		{
+			await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: commands },
+			);
+		}
+		else
+		{
+			await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: commands },
+			);
+		}
 
 		console.log('Successfully registered application commands.');
 	}
