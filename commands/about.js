dc5fe0a5569e80234c6c6f648d9aa5d4730a8874
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2021-09-20 12:04:49 -0500

    About command

---
+++ commands/about.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require("discord.js");
+const { inviteUrl } = require("../config.json");
+
+module.exports =
+{
+	async execute(interaction)
+	{
+		let embed = new MessageEmbed()
+			.setAuthor("EzROI", interaction.client.user.displayAvatarURL())
+			.setDescription(`About Me`)
+			.addField("Primary Focus", `EzROI is a helpful math both that attempts to provide accurate data to determine the ROI (Return of Investment) of the GPU provided in terms of mining through Nicehash. By coupling Nicehash data with Coinbase's currency API, the most accurate data at the current moment is provided for every action. While fiat conversions are the main focus, some cryptocurrencies can be provided in the data and may return good results. Due to the extreme amount of cryptocurrencies available, only the most popular cryptos will be accounted for (BTC, ETH, XLM, ADA, ect.).`, false)
+			.addField("What It **CAN** Do", 
+			`\u2022 Help you make a decision
+			\u2022 List information about GPUs
+			\u200B \u200B \u200B \u200B \u200B \u25E6 Includes wattage and hashrates`
+			, true)
+			.addField("What It **CAN NOT** Do", 
+			`\u2022 Display graphs for crypto charts
+			\u2022 Crypto or fiat conversions on demand
+			\u2022 Suggest GPUs to purchase`
+			, true)
+
+		await interaction.reply({ embeds: [embed], ephemeral: false});
+	},
+
+	data: new SlashCommandBuilder()
+		.setName('about')
+		.setDescription('Information about me.')
+};
